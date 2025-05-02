bad996347480b06563474e19e9a8ea9d872b3058
Tech-master1234 <manojbarathi1234@gmail.com>
2025-05-02 22:50:01 +0530

    Custom Pages for status codes

--- Learning Basics/Django/myapp/myapp/settings.py
+++ Learning Basics/Django/myapp/myapp/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&69o3ual8$80t+%!o(z+i6c6vwgzvj_2c5cs_(=)al#hlj!mzf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
