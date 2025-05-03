94d23d5814cafc3a0242f9e71c37c0ae8b95a9af
Tech-master1234 <manojbarathi1234@gmail.com>
2025-05-03 22:24:08 +0530

    Migrations

--- Learning Basics/Django/myapp/blog/models.py
+++ Learning Basics/Django/myapp/blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     img_url = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
