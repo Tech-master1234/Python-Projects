30078390b81edf43127a41b8855d8fdc5cf2e588
Tech-master1234 <manojbarathi1234@gmail.com>
2025-05-04 20:43:26 +0530

    Showing data from sql to html

--- Learning Basics/Django/myapp/blog/views.py
+++ Learning Basics/Django/myapp/blog/views.py
@@ -2,25 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
-
+from .models import Post
 # Create your views here.
-posts = [
-    {'id':1,'title':'Post 1','content':'Content of post 1'},
-    {'id':2,'title':'Post 2','content':'Content of post 2'},
-    {'id':3,'title':'Post 3','content':'Content of post 3'},
-    {'id':4,'title':'Post 4','content':'Content of post 4'},
-]
+# posts = [
+#     {'id':1,'title':'Post 1','content':'Content of post 1'},
+#     {'id':2,'title':'Post 2','content':'Content of post 2'},
+#     {'id':3,'title':'Post 3','content':'Content of post 3'},
+#     {'id':4,'title':'Post 4','content':'Content of post 4'},
+# ]
+blog_title = 'Todays picks'
+site_title = 'Blog Posts'
 def index(requests):
-    blog_title = 'Todays picks'
-    site_title = 'Blog Posts'
-
+    posts = Post.objects.all()
     return render(requests,'index.html',{'blog_title':blog_title,'site_title' : site_title,'posts':posts})
 
 def detail(requests,post_id):
-    post = next((item for item in posts if item['id']==int(post_id)),None)
-    logger = logging.getLogger("TESTING")
-    logger.debug(f'Post variable is {post}')
-    return render(requests,'detail.html',{'post':post})
+    return render(requests,'detail.html',{'post_id':post_id})
 
 def old_url_redirect(requests):
     return redirect(reverse('blog:new_page_url'))
