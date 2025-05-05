ff09f19f058ca627d45f6c37c335ff82b3fe9dc5
Tech-master1234 <manojbarathi1234@gmail.com>
2025-05-05 22:44:38 +0530

    Creating slug urls and storing them in DB

--- Learning Basics/Django/myapp/blog/urls.py
+++ Learning Basics/Django/myapp/blog/urls.py
@@ -6,10 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path("", views.index, name = "index"),
     path("post/details/<str:post_id>", views.detail, name="detail"),
-    # path("post/details/<int:post_id>", views.detail, name="detail")
     path("old_url", views.old_url_redirect, name='old_url'),
-    # path("new_url", views.new_url_view, name='new_page_url')
-    # path("new_url", views.new_url_view, name='new_page_url_something')
     path("new_url_new", views.new_url_view, name='new_page_url')
 
 
