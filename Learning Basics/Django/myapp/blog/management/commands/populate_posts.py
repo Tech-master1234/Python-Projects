b706300333caa9806517075e25bfaff2108189f4
Tech-master1234 <manojbarathi1234@gmail.com>
2025-05-10 22:40:33 +0530

    Added Post Categories table,Inserted values

--- Learning Basics/Django/myapp/blog/management/commands/populate_posts.py
+++ Learning Basics/Django/myapp/blog/management/commands/populate_posts.py
@@ -1,7 +1,7 @@
 from typing import Any
-from blog.models import Post
+from blog.models import Post, Category
 from django.core.management.base import BaseCommand
-
+import random
 
 class Command(BaseCommand):
     help = "This commands inserts post data"
@@ -79,7 +79,11 @@ class Command(BaseCommand):
             "https://picsum.photos/id/20/800/400",
         ]
         
+        # Get Categories
+        categories = Category.objects.all()
+
         for title, content, img_url in zip(titles, contents, img_urls):
-            Post.objects.create(title=title, content=content, img_url=img_url)
+            category = random.choice(categories)
+            Post.objects.create(title=title, content=content, img_url=img_url, category = category)
 
         self.stdout.write(self.style.SUCCESS("Completed inserting Data!"))
